7263e1b43574946da7146730802df42ff42c9f63
DanJ <danj@axoid.net>
2021-04-19 01:01:24 +0200

    added extra info

--- main.ts
+++ main.ts
@@ -4,15 +4,24 @@
 
 console.log("hello from Deno!");
 
-var count = 0;
+const serverstart = new Date().toLocaleString();
+var visitcount = 0;
+var lastvisit = "never"
 
 console.log("starting web server");
 addEventListener("fetch", (event) => {
 	
-	count++;
+	visitcount++;
 	
-	const response = new Response("Hello World! this page has been visited " + count + " times!!", {
+	const response = new Response(
+		`Hello World! 
+		this page has been visited ${visitcount} times!!
+		
+		last visit was at: ${lastvisit}
+		server started at: ${serverstart}`, {
 		headers: { "content-type": "text/plain" },
 	});
+	
+	lastvisit = new Date().toLocaleString();
 	event.respondWith(response);
 });
